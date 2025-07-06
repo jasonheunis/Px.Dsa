521c6afd0828216b5c0d6c27b2c80e766e10ad58
jasonheunis <jasonheunis@gmail.com>
2025-07-06 11:46:03 +0200

    Update packages and restructure solution files
    - Updated package versions in `Directory.Packages.props`, including `Aspire.Hosting.SqlServer`, `Microsoft.Build.Tasks.Core`, and `OpenTelemetry.Api`.
    - Modified `FSH.Starter.sln` to include new projects and updated to Visual Studio 17 format.
    - Added `OpenTelemetry.Api` references in multiple `.csproj` files.
    - Updated connection strings in `appsettings.Development.json` and `appsettings.json` for MSSQL.
    - Refactored database connection parameters in `Program.cs` to use SQL Server.

--- src/aspire/Host/Program.cs
+++ src/aspire/Host/Program.cs
@@ -9,12 +9,13 @@ builder.AddContainer("prometheus", "prom/prometheus")
        .WithBindMount("../../../compose/prometheus", "/etc/prometheus", isReadOnly: true)
        .WithHttpEndpoint(port: 9090, targetPort: 9090);
 
-var username = builder.AddParameter("pg-username", "admin");
-var password = builder.AddParameter("pg-password", "admin");
+var username = builder.AddParameter("mssql-username", "Px-Dsa-Dev");
+var password = builder.AddParameter("mssql-password", "sFbRBnQgGv7YMt6TuxGNXcwh7zhWpNpA");
+var port = builder.AddParameter("mssql-port", "1433");
 
-var database = builder.AddPostgres("db", username, password, port: 5432)
-    .WithDataVolume()
-    .AddDatabase("fullstackhero");
+var database = builder.AddSqlServer("mssql", password, 1433)
+                      .WithDataVolume()
+                      .AddDatabase("Px-Dsa");
 
 var api = builder.AddProject<Projects.Server>("webapi")
     .WaitFor(database);
